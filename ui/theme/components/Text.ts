dffac90ea5bd7be371d034b6d6cc99554b60a749
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2023-04-05 17:13:01 +0200

    feat: theme from tdb

---
+++ ui/theme/components/Text.ts
@@ -0,0 +1,18 @@
+const Text = {
+  variants: {
+    highlight: {
+      textUnderlineOffset: "0px",
+      textDecorationLine: "underline",
+      textDecorationStyle: "solid",
+      textDecorationColor: "yellow.300",
+      textDecorationThickness: "5px",
+    },
+    card: {
+      p: 8,
+      bg: "#F9F8F6",
+      display: "block",
+    },
+  },
+};
+
+export { Text };
