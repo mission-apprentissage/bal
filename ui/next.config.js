d8a5bae3ca81aa91e4427f88fc660b3bd3b2d50a
Antoine Bigard <bigard.antoine@gmail.com>
2023-04-11 10:31:55 +0200

    fix: next conf plausible

--- ui/next.config.js
+++ ui/next.config.js
@@ -3,13 +3,52 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require("path");
 
+
+
+function inline(value) {
+  return value.replace(/\s{2,}/g, " ").trim();
+}
+
+const contentSecurityPolicy = `
+      default-src 'self' https://plausible.io;
+      base-uri 'self';
+      block-all-mixed-content;
+      font-src 'self'  https: data:;
+      frame-ancestors 'self' https://bal.apprentissage.beta.gouv.fr;
+      frame-src 'self' https://plausible.io https://bal.apprentissage.beta.gouv.fr;
+      img-src 'self' https://www.notion.so data: ${
+        process.env.NEXT_PUBLIC_ENV !== "production" ? "" : ""
+      };
+      object-src 'none';
+      script-src 'self' https://plausible.io ${
+        process.env.NEXT_PUBLIC_ENV === "dev" ? "'unsafe-eval' 'unsafe-inline'" : ""
+      };
+      script-src-attr 'none';
+      style-src 'self' https:  https: *.plausible.io 'unsafe-inline';
+      connect-src 'self' https://geo.api.gouv.fr/ https://plausible.io;
+      upgrade-insecure-requests;
+`;
+
 const nextConfig = {
   transpilePackages: ['shared'],
   experimental: {
     appDir: true,
     outputFileTracingRoot: path.join(__dirname, '../')
   },
-  output: 'standalone'
+  output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: inline(contentSecurityPolicy),
+          },
+        ],
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
