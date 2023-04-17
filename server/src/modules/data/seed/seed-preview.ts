9337f83223b25a55a37e425ef20f6853e6520575
Antoine Bigard <bigard.antoine@gmail.com>
2023-04-17 14:32:07 +0200

    chore: add config preview

--- server/src/modules/data/seed/seed-preview.ts
+++ server/src/modules/data/seed/seed-preview.ts
@@ -1,11 +1,12 @@
 
+import { config } from "../../../../config/config";
 import { createUser } from "../../actions/users.actions";
 
 export const seedPreview = async () => {
     await createUser({ email: config.tests.testUserName, password: config.tests.testUserPwd });
-  await createUser({
-    email: "test-admin@bal.apprentissage.beta.gouv.fr",
-    password: "test",
-    isAdmin: true,
-  });
+    await createUser({
+        email: config.tests.testAdminName,
+        password: config.tests.testAdminPwd,
+        isAdmin: true,
+    });
 };
