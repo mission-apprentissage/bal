3a55e7299149489bacc8f426f7a776a72de4ddff
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2023-04-24 17:19:07 +0200

    feat(ui): confirmation message upload

--- ui/app/admin/import/page.tsx
+++ ui/app/admin/import/page.tsx
@@ -49,11 +49,12 @@ const AdminImportPage = () => {
     try {
       const formData = new FormData();
       formData.append("file", data.file);
-      const response = await api.post("/admin/upload", formData, {
+
+      await api.post("/admin/upload", formData, {
         params: { type: data.type_document },
       });
 
-      console.log(response);
+      setStatus({ error: false, message: "Fichier importé avec succès." });
     } catch (error) {
       const axiosError = error as AxiosError<IResError>;
       setStatus({ error: true, message: axiosError.response?.data.message });
@@ -75,71 +76,76 @@ const AdminImportPage = () => {
         <Heading as="h2" fontSize="2xl" mb={[3, 6]}>
           Importer un fichier
         </Heading>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <Box>
-            <FormControl isInvalid={!!errors.type_document} mb={5}>
-              <FormLabel>Type de document</FormLabel>
-              <Select
-                isInvalid={!!errors.type_document}
-                placeholder="Choisir un type de document"
-                {...register("type_document", {
-                  required: "Obligatoire",
-                  validate: (value) => {
-                    return value && UPLOAD_TYPES.includes(value);
-                  },
-                })}
-              >
-                {UPLOAD_TYPES.map((type) => (
-                  <option key={type}>{type}</option>
-                ))}
-              </Select>
-              <FormErrorMessage>
-                {errors.type_document?.message}
-              </FormErrorMessage>
-            </FormControl>
 
-            <FormControl isInvalid={!!errors.file} mb={5}>
-              <FormLabel>Fichier</FormLabel>
-              <Input
-                {...register("file", {
-                  required: "Obligatoire",
-                })}
-                hidden
-              />
-              <Dropzone
-                options={{
-                  maxFiles: 1,
-                  onDrop: (files) => {
-                    setValue("file", files[0]);
-                  },
-                  onDropRejected: () => {
-                    console.log("rejected");
-                  },
-                  accept: {
-                    // TODO [Métier] RUSH SIFA 2022
-                    // "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": [".xlsx"],
-                    // "application/vnd.ms-excel": [".xls", ".csv"],
-                    "text/csv": [".csv"],
-                  },
-                  maxSize: FILE_SIZE_LIMIT,
-                }}
-                isLoading={isSubmitting}
-              />
-              <FormErrorMessage>{errors.file?.message}</FormErrorMessage>
-            </FormControl>
+        {status?.message && !status.error ? (
+          <Text>{status.message}</Text>
+        ) : (
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Box>
+              <FormControl isInvalid={!!errors.type_document} mb={5}>
+                <FormLabel>Type de document</FormLabel>
+                <Select
+                  isInvalid={!!errors.type_document}
+                  placeholder="Choisir un type de document"
+                  {...register("type_document", {
+                    required: "Obligatoire",
+                    validate: (value) => {
+                      return value && UPLOAD_TYPES.includes(value);
+                    },
+                  })}
+                >
+                  {UPLOAD_TYPES.map((type) => (
+                    <option key={type}>{type}</option>
+                  ))}
+                </Select>
+                <FormErrorMessage>
+                  {errors.type_document?.message}
+                </FormErrorMessage>
+              </FormControl>
 
-            {status?.error && (
-              <Text color="error" mt={8}>
-                <AlertRounded mb="0.5" /> {status?.message}
-              </Text>
-            )}
-          </Box>
-          <HStack spacing="4w" mt={8}>
-            <Button variant="primary" type="submit">
-              Envoyer
-            </Button>
-          </HStack>
-        </form>
+              <FormControl isInvalid={!!errors.file} mb={5}>
+                <FormLabel>Fichier</FormLabel>
+                <Input
+                  {...register("file", {
+                    required: "Obligatoire",
+                  })}
+                  hidden
+                />
+                <Dropzone
+                  options={{
+                    maxFiles: 1,
+                    onDrop: (files) => {
+                      setValue("file", files[0]);
+                    },
+                    onDropRejected: () => {
+                      console.log("rejected");
+                    },
+                    accept: {
+                      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet":
+                        [".xlsx"],
+                      "application/vnd.ms-excel": [".xls", ".csv"],
+                      "text/csv": [".csv"],
+                    },
+                    maxSize: FILE_SIZE_LIMIT,
+                  }}
+                  isLoading={isSubmitting}
+                />
+                <FormErrorMessage>{errors.file?.message}</FormErrorMessage>
+              </FormControl>
+
+              {status?.error && (
+                <Text color="error" mt={8}>
+                  <AlertRounded mb="0.5" /> {status?.message}
+                </Text>
+              )}
+            </Box>
+            <HStack spacing="4w" mt={8}>
+              <Button variant="primary" type="submit">
+                Envoyer
+              </Button>
+            </HStack>
+          </form>
+        )}
       </Flex>
     </>
   );
