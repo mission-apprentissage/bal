9d1e93e7cc8d874cd1f01a8917a000f0fddb7e42
Antoine Bigard <bigard.antoine@gmail.com>
2023-04-26 15:06:41 +0200

    fix(ui): metadata

--- ui/app/layout.tsx
+++ ui/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import "react-notion-x/src/styles.css";
 
+import { Metadata } from "next";
 import { headers } from "next/headers";
 import { PropsWithChildren } from "react";
 
@@ -25,6 +26,21 @@ async function getSession() {
 
   return session;
 }
+
+export const metadata: Metadata = {
+  viewport: "width=device-width, initial-scale=1",
+  icons: {
+    icon: [{ url: "/favicon.ico" }, { url: "/favicon.svg" }],
+    apple: [{ url: "/apple-touch-icon.png" }],
+    // other: {
+    //   rel: '',
+    //   url: '/android-chrome-192x192.png',
+    //   url: '/android-chrome-512x512.png',
+    // },
+  },
+  title: "BAL",
+  description: "BAL apprentissage",
+};
 
 export default async function RootLayout({ children }: PropsWithChildren) {
   const session = await getSession();
