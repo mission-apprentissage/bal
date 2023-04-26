96ff88ed2710399e51413404a4b5dc346d8e1d4b
Antoine Bigard <bigard.antoine@gmail.com>
2023-04-26 14:41:56 +0200

    fix: cypres target

--- cypress/e2e/admin/canSeeUploadPage.cy.js
+++ cypress/e2e/admin/canSeeUploadPage.cy.js
@@ -3,7 +3,7 @@ describe("canSeeUploadPage", () => {
     cy.viewport(1381, 992);
     cy.visit("https://bal-recette.apprentissage.beta.gouv.fr/");
     cy.get("#menu-button-\\:Rlab5femH1\\:").click();
-    // cy.get("#menu-list-:r1:-menuitem-:r3:").type("Import de fichier");
+    cy.get("[data-id='menuitem\\:importfile']").type("Import de fichier");
   });
 });
 //# recorderSourceMap=BCBDBEBFB
