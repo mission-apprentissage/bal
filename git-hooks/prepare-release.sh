c8a6e1d16649744ee3abc40b8b37b5fb7855f746
Antoine Bigard <bigard.antoine@gmail.com>
2023-05-04 14:06:56 +0200

    fix: concurrency pull

--- git-hooks/prepare-release.sh
+++ git-hooks/prepare-release.sh
@@ -37,3 +37,6 @@ echo "Bump app version in env.ini : $next_version"
 
 sed -i "s/default:.*/default: $next_version/" ".github/workflows/_deploy.yml"
 echo "Bump app version in _deploy.yml : $next_version"
+
+## Concurrency mode 
+git pull
