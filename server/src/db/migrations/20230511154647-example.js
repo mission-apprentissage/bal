20417b2e5a54a21fbbe5f321a1f64af7585929a8
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2023-05-12 15:36:12 +0200

    feat: migrate-mongo

---
+++ server/src/db/migrations/20230511154647-example.js
@@ -0,0 +1,6 @@
+export const up = async (db, _client) => {
+  // TODO write your migration here.
+  // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
+  // Example:
+  await db.collection("users").find();
+};
