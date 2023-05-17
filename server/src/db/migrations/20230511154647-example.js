5c81be3ce4f5f5045f5151dee495c27530d3abc7
Antoine Bigard <bigard.antoine@gmail.com>
2023-05-17 17:29:50 +0200

    chore: rm log

--- server/src/db/migrations/20230511154647-example.js
+++ server/src/db/migrations/20230511154647-example.js
@@ -2,6 +2,5 @@ export const up = async (db, _client) => {
   // TODO write your migration here.
   // See https://github.com/seppevs/migrate-mongo/#creating-a-new-migration-script
   // Example:
-  const test = await db.collection("users").find().toArray();
-  console.log(test);
+  await db.collection("users").find().toArray();
 };
