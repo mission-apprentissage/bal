fd0750c1c01d5aaea32fec607cd19dc6d652fec7
Moroine <moroine.bentefrit@gmail.com>
2023-06-12 11:56:28 +0200

    fix: enforce platform build

--- .infra/scripts/release/build-images.sh
+++ .infra/scripts/release/build-images.sh
@@ -11,6 +11,7 @@ docker build . \
 
 echo "Build ui:$next_version ..."
 docker build . -f "ui/Dockerfile" \
+        --platform linux/amd64 \
         --tag ghcr.io/mission-apprentissage/mna_bal_ui:"$next_version" \
         --build-arg VERSION=$next_version \
         --label "org.opencontainers.image.source=https://github.com/mission-apprentissage/bal" \
@@ -19,6 +20,7 @@ docker build . -f "ui/Dockerfile" \
 
 echo "Building server:$next_version ..."
 docker build . -f "server/Dockerfile" \
+          --platform linux/amd64 \
           --progress=plain \
           --tag ghcr.io/mission-apprentissage/mna_bal_server:"$next_version" \
           --build-arg VERSION=$next_version \
