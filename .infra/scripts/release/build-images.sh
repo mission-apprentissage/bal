4d61b948764728a15da0a8a1c872a1ebedf042c0
Moroine <moroine.bentefrit@gmail.com>
2023-06-12 12:02:47 +0200

    chore: support pushing to local registry

--- .infra/scripts/release/build-images.sh
+++ .infra/scripts/release/build-images.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 next_version="${1}"
+registry=${2:?"Veuillez préciser le registry"}
 
 echo "Création des images docker (docker build)"
 echo "Build deps"
@@ -12,7 +13,7 @@ docker build . \
 echo "Build ui:$next_version ..."
 docker build . -f "ui/Dockerfile" \
         --platform linux/amd64 \
-        --tag ghcr.io/mission-apprentissage/mna_bal_ui:"$next_version" \
+        --tag $registry/mission-apprentissage/mna_bal_ui:"$next_version" \
         --build-arg VERSION=$next_version \
         --label "org.opencontainers.image.source=https://github.com/mission-apprentissage/bal" \
         --label "org.opencontainers.image.description=Ui bal" \
@@ -22,7 +23,7 @@ echo "Building server:$next_version ..."
 docker build . -f "server/Dockerfile" \
           --platform linux/amd64 \
           --progress=plain \
-          --tag ghcr.io/mission-apprentissage/mna_bal_server:"$next_version" \
+          --tag $registry/mission-apprentissage/mna_bal_server:"$next_version" \
           --build-arg VERSION=$next_version \
           --label "org.opencontainers.image.source=https://github.com/mission-apprentissage/bal" \
           --label "org.opencontainers.image.description=Server bal" \
--- .infra/scripts/release/push-git.sh
+++ .infra/scripts/release/push-git.sh
@@ -3,15 +3,24 @@ set -euo pipefail
 
 next_version="${1}"
 
-## Concurrency mode 
-git add  CHANGELOG.md \
-CHANGELOG.md \
-.github/workflows/_deploy.yml \
-.infra/.env \
-package.json \
-server/package.json \
-shared/package.json \
-ui/package.json
+if output=$(git status --porcelain) && [ -z "$output" ]; then
+  ## Concurrency mode 
+  git add  CHANGELOG.md \
+  CHANGELOG.md \
+  .github/workflows/_deploy.yml \
+  .infra/.env \
+  package.json \
+  server/package.json \
+  shared/package.json \
+  ui/package.json
 
-git commit -m "chore(release): bump $next_version [skip ci]"
-git push --tags https://$GH_TOKEN@github.com/mission-apprentissage/bal.git main --force
+  git commit -m "chore(release): bump $next_version [skip ci]"
+else 
+  echo "Skipping commit: Working tree is not clean"
+fi
+
+if [ ! -z "${CI:-}" ]; then
+  git push --tags https://$GH_TOKEN@github.com/mission-apprentissage/bal.git main --force
+else
+  echo "Skipping push: Not in CI"
+fi
