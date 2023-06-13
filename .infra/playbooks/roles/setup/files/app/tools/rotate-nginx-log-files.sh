b2fa8cd2d13e36b61efee73a39131ca7a3960111
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-06-13 17:37:22 +0200

    feat: use fluentd for logs

--- .infra/playbooks/roles/setup/files/app/tools/rotate-nginx-log-files.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-echo "****************************"
-echo "[$(date +'%Y-%m-%d_%H%M%S')] Running ${BASH_SOURCE[0]} $*"
-echo "****************************"
-docker exec $(docker ps -q -f name=bal_reverse_proxy --latest) bash -c "/usr/sbin/logrotate -s /data/status /etc/logrotate.conf"
