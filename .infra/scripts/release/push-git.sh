e74aae703b1da33e97b90e020c425676b6269903
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-06-19 16:57:34 +0200

    fix: push git

--- .infra/scripts/release/push-git.sh
+++ .infra/scripts/release/push-git.sh
@@ -3,23 +3,16 @@ set -euo pipefail
 
 next_version="${1}"
 
-if output=$(git status --porcelain) && [ -z "$output" ]; then
-  ## Concurrency mode 
-  git add  CHANGELOG.md \
-  CHANGELOG.md \
+if [ ! -z "${CI:-}" ]; then
+  git add CHANGELOG.md \
   .github/workflows/_deploy.yml \
-  .infra/.env \
+  .infra/.env_docker_compose \
   package.json \
   server/package.json \
   shared/package.json \
   ui/package.json
 
   git commit -m "chore(release): bump $next_version [skip ci]"
-else 
-  echo "Skipping commit: Working tree is not clean"
-fi
-
-if [ ! -z "${CI:-}" ]; then
   git push --tags https://$GH_TOKEN@github.com/mission-apprentissage/bal.git $GITHUB_REF_NAME --force
 else
   echo "Skipping push: Not in CI"
