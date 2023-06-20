5bbda979af615dcba76428a0893909a706bc5946
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-06-20 13:31:54 +0200

    chore: add vault merge tool

---
+++ .infra/scripts/vault/merge-vault.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+readonly SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+readonly VAULT_FILE="${SCRIPT_DIR}/../../vault/vault.yml"
+
+ancestor_version=$1
+current_version=$2
+other_version=$3
+conflict_marker_size=$4
+merged_result_pathname=$5
+
+ancestor_tempfile=$(mktemp tmp.XXXXXXXXXX)
+current_tempfile=$(mktemp tmp.XXXXXXXXXX)
+other_tempfile=$(mktemp tmp.XXXXXXXXXX)
+
+delete_tempfiles() {
+    rm -f "$ancestor_tempfile" "$current_tempfile" "$other_tempfile"
+}
+trap delete_tempfiles EXIT
+
+ansible-vault decrypt --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh" --output "$ancestor_tempfile" "$ancestor_version"
+ansible-vault decrypt --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh" --output "$current_tempfile" "$current_version"
+ansible-vault decrypt --vault-password-file="${SCRIPT_DIR}/get-vault-password-client.sh" --output "$other_tempfile" "$other_version"
+
+git merge-file "$current_tempfile" "$ancestor_tempfile" "$other_tempfile"
+
+ansible-vault encrypt --output "$current_version" "$current_tempfile"
