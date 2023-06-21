ca78c0702d693eef30ad39343fd5101898f4b7b2
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2023-06-21 09:33:27 +0200

    Decoupage du script prepare release (#122)

---
+++ .infra/scripts/release/build-images.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -euo pipefail
+
+next_version="${1}"
+
+echo "Création des images docker (docker build)"
+echo "Build deps"
+docker build . \
+        --tag mna_bal_deps_installer:local \
+        --platform linux/amd64
+
+echo "Build ui:$next_version ..."
+docker build . -f "ui/Dockerfile" \
+        --tag ghcr.io/mission-apprentissage/mna_bal_ui:"$next_version" \
+        --build-arg VERSION=$next_version \
+        --label "org.opencontainers.image.source=https://github.com/mission-apprentissage/bal" \
+        --label "org.opencontainers.image.description=Ui bal" \
+        --label "org.opencontainers.image.licenses=MIT"
+
+echo "Building server:$next_version ..."
+docker build . -f "server/Dockerfile" \
+          --progress=plain \
+          --tag ghcr.io/mission-apprentissage/mna_bal_server:"$next_version" \
+          --build-arg VERSION=$next_version \
+          --label "org.opencontainers.image.source=https://github.com/mission-apprentissage/bal" \
+          --label "org.opencontainers.image.description=Server bal" \
+          --label "org.opencontainers.image.licenses=MIT"
+
+sleep 3
---
+++ .infra/scripts/release/push-images.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -euo pipefail
+
+next_version="${1}"
+
+echo "Push des images locales sur le registry"
+echo "Pushing ui:$next_version ..."
+docker push ghcr.io/mission-apprentissage/mna_bal_ui:"$next_version"
+sleep 3
+echo "Pushing server:$next_version ..."
+docker push ghcr.io/mission-apprentissage/mna_bal_server:"$next_version"
+
+sed -i "s/app_version=.*/app_version=$next_version/" ".infra/env.ini"
+echo "Bump app version in env.ini : $next_version"
+
+sed -i "s/default:.*/default: $next_version/" ".github/workflows/_deploy.yml"
+echo "Bump app version in _deploy.yml : $next_version"
