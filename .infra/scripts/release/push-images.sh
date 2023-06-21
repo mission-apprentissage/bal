6386e9b13f2d583c1ad5ae553d0ae2ce6e6ea4b0
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2023-06-21 09:39:18 +0200

    Enforce workspace package reference (#124)

--- .infra/scripts/release/push-images.sh
+++ .infra/scripts/release/push-images.sh
@@ -11,8 +11,11 @@ sleep 3
 echo "Pushing server:$next_version ..."
 docker push $registry/mission-apprentissage/mna_bal_server:"$next_version"
 
-sed -i "s/app_version=.*/app_version=$next_version/" ".infra/env.ini"
-echo "Bump app version in env.ini : $next_version"
+sed -i '' "s/registry=.*/registry=$registry/" ".infra/.env_docker_compose"
+echo "Bump registry in .infra/.env_docker_compose : $registry"
 
-sed -i "s/default:.*/default: $next_version/" ".github/workflows/_deploy.yml"
+sed -i '' "s/app_version=.*/app_version=$next_version/" ".infra/.env_docker_compose"
+echo "Bump app version in .infra/.env_docker_compose : $next_version"
+
+sed -i '' "s/default:.*/default: $next_version/" ".github/workflows/_deploy.yml"
 echo "Bump app version in _deploy.yml : $next_version"
