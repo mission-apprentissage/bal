ccc172d4bb477e06aa0dd3fb0f9c4036092cb487
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-07-06 22:31:21 +0200

    fix: ci

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,14 +1,10 @@
-import { config } from "dotenv";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
-
-config({
-  path: "./.env.test",
-});
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    include: ["tests/**/*.test.ts"],
+    include: ["./tests/**/*.test.ts"],
+    setupFiles: ["./tests/setup.ts"],
   },
 });
