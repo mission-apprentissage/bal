379fc90a72a4e31e819ec2b204fce973bc3d4134
Moroine <moroine.bentefrit@beta.gouv.fr>
2023-07-13 17:46:53 +0200

    chore: fix migration settings

--- server/src/modules/jobs/migrations/migrations.ts
+++ server/src/modules/jobs/migrations/migrations.ts
@@ -33,7 +33,7 @@ const myConfig = {
   changelogCollectionName: "migrations",
 
   // The file extension to create migrations and search for in migration dir
-  migrationFileExtension: ".ts",
+  migrationFileExtension: ".js",
 
   // Enable the algorithm to create a checksum of the file contents and use that in the comparison to determin
   // if the file should be run.  Requires that scripts are coded to be run multiple times.
