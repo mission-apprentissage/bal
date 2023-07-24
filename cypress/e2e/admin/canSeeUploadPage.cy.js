6fe8c7a51abb17cbe3902c220d3db84d688dea1f
Antoine Bigard <6003532+antoinebigard@users.noreply.github.com>
2023-07-24 09:56:52 +0200

    feat: cypress script with stop condition [skip ci] (#250)
    * chore: wip cypress conf
    
    * chore: wip cypress research
    
    * fix: rename to mjs
    
    * feat: convert chrome recorder with stop condition
    
    * chore: quick readme for chrome recorder

--- cypress/e2e/admin/canSeeUploadPage.cy.js
+++
@@ -1,9 +0,0 @@
-describe("canSeeUploadPage", () => {
-  it("tests canSeeUploadPage", () => {
-    cy.viewport(2064, 992);
-    cy.visit("https://bal-recette.apprentissage.beta.gouv.fr/");
-    cy.get("#menu-button-\\:Rlab5femH1\\:").click();
-    cy.get("[data-id='menuitem\\:importfile']").should("exist");
-  });
-});
-//# recorderSourceMap=BCBDBEBFB
