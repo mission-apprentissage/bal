2470d86a1653da24b5d2b27dcf29a7cb5dd5573d
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2023-09-14 16:11:49 +0200

    fix: table pagination align

--- ui/components/table/Table.tsx
+++ ui/components/table/Table.tsx
@@ -1,10 +1,18 @@
 import { Box } from "@mui/material";
+import { styled } from "@mui/material/styles";
 import { DataGrid, DataGridProps, frFR, GridValidRowModel } from "@mui/x-data-grid";
+
+const StyledDataGrid = styled(DataGrid)(({ theme }) => ({
+  "& .MuiTablePagination-selectLabel": {
+    margin: theme.spacing(0),
+  },
+}));
 
 export const Table = <R extends GridValidRowModel = any>(props: DataGridProps<R>) => {
   return (
     <Box my={2}>
-      <DataGrid
+      <StyledDataGrid
+        // @ts-ignore
         getRowId={(row) => row?._id ?? row.id}
         rowHeight={60}
         localeText={frFR.components.MuiDataGrid.defaultProps.localeText}
