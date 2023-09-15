58f3e52a56d43dcdb33eca6a75eedd8502df58f6
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2023-09-15 14:28:25 +0200

    fix: filter

--- server/src/db/migrations/20230915092918-remove_person_organisation.ts
+++ server/src/db/migrations/20230915092918-remove_person_organisation.ts
@@ -2,7 +2,7 @@ import { Db, MongoClient } from "mongodb";
 
 export const up = async (db: Db, _client: MongoClient) => {
   await db.collection("persons").updateMany(
-    {},
+    { organisation: { $exists: true } },
     { $unset: { organisation: true } },
     {
       bypassDocumentValidation: true,
