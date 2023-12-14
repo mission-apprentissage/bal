3c5ca38851941ed003e855709e651f77d132030b
Antoine Bigard <bigard.antoine@gmail.com>
2023-12-14 07:59:11 +0100

    feat: add nir apprenti cascade

--- ui/app/cerfa/controls/apprentiNir.control.ts
+++ ui/app/cerfa/controls/apprentiNir.control.ts
@@ -35,7 +35,7 @@ export const apprentiNirControl: CerfaControl[] = [
         return {};
       }
 
-      if (nir?.substring(1, 3) !== dateNaissance.substring(2, 4)) {
+      if (!isEmpty(dateNaissance) && nir?.substring(1, 3) !== dateNaissance.substring(2, 4)) {
         return {
           error: "L'année de naissance indiquée ne correspond pas aux 2e et 3e chiffres du NIR",
         };
@@ -45,13 +45,35 @@ export const apprentiNirControl: CerfaControl[] = [
         return {};
       }
 
-      if (nir?.substring(3, 5) !== dateNaissance.substring(5, 7)) {
+      if (!isEmpty(dateNaissance) && nir?.substring(3, 5) !== dateNaissance.substring(5, 7)) {
         return {
           error: "Le mois de naissance indiqué ne correspond pas aux 4e et 5e chiffres du NIR",
         };
       }
 
-      return {};
+      if (!nir.startsWith("2") && !nir.startsWith("1")) {
+        return {
+          error: "Le NIR doit commencer par 1 ou 2",
+        };
+      }
+
+      const anneeNir = parseInt(nir?.substring(1, 3));
+      const moisNir = nir?.substring(3, 5);
+      const annee = anneeNir < 15 ? `20${anneeNir}` : `19${anneeNir}`;
+
+      return {
+        cascade: {
+          ...(isEmpty(sexe) ? { "apprenti.sexe": { value: nir.startsWith("1") ? "M" : "F", cascade: false } } : {}),
+          ...(isEmpty(dateNaissance)
+            ? {
+                "apprenti.dateNaissance": {
+                  value: `${annee}-${moisNir}-01`,
+                  cascade: false,
+                },
+              }
+            : {}),
+        },
+      };
     },
   },
 ];
