5f1a6b2b9dca179506236ae0cc619a67e7fc11e0
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2024-01-30 09:15:28 +0100

    fix: durée travail

--- ui/app/cerfa/controls/dureeTravail.control.ts
+++ ui/app/cerfa/controls/dureeTravail.control.ts
@@ -4,7 +4,13 @@ export const dureeTravailControl: CerfaControl[] = [
   {
     deps: ["contrat.dureeTravailHebdoHeures", "contrat.dureeTravailHebdoMinutes"],
     process: ({ values }) => {
-      if (values.contrat.dureeTravailHebdoHeures >= 40 && values.contrat.dureeTravailHebdoMinutes) {
+      const minutes = parseInt(values.contrat.dureeTravailHebdoMinutes);
+      const heures = parseInt(values.contrat.dureeTravailHebdoHeures);
+
+      if (minutes > 0 && heures >= 40) {
+        return { error: "la durée de travail hebdomadaire en heures ne peut excéder 40h" };
+      }
+      if (heures > 40) {
         return { error: "la durée de travail hebdomadaire en heures ne peut excéder 40h" };
       }
     },
