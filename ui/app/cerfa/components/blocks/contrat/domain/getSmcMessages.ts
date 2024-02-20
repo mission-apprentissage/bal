2fe4f8918c40e1aea6e64c8f9c2dc58c6f9a1d39
David Dela Cruz <david.dela.cruz@beta.gouv.fr>
2024-02-20 17:15:56 +0100

    fix: message smc

--- ui/app/cerfa/components/blocks/contrat/domain/getSmcMessages.ts
+++ ui/app/cerfa/components/blocks/contrat/domain/getSmcMessages.ts
@@ -7,27 +7,25 @@ const getMessages = (url?: string | null): InformationMessage[] => {
   return [
     {
       type: "assistive",
-      content: `Lorsque l'apprenti a 21 ans ou plus, si le SMC existe et que son montant est plus avantageux pour l'apprenti, son application est obligatoire.
+      content: `Si vous ne connaissez pas le SMC applicable, consultez votre convention collective.
 
 ${url ? `[Votre convention collective](${url})` : ""}`,
       collapse: {
         label: "En savoir plus",
-        content: `La base sur laquelle est calculée la rémunération de l'apprenti correspond au salaire le plus avantageux entre le salaire minimum de croissance (SMIC, qui correspond au salaire minimum légal que le salarié doit percevoir), et le salaire minimum conventionnel (SMC) indiqué dans la convention collective applicable à votre activité.
-     
-Si salaire minimum conventionnel est supérieur au SMIC, c’est le salaire minimum conventionnel qui s’applique. En revanche, lorsque le montant du salaire minimum conventionnel est inférieur au SMIC, vous devez verser au minimum un salaire qui se base sur le montant du SMIC.`,
+        content: `L'information figure en général dans la partie "Salaires". Vous devez vous référer à la position du poste occupé par l'apprenti dans la classification hiérarchique pour  déterminer la rémunération minimum applicable à l’apprenti à partir du salaire de base et du pourcentage appliqué.
+
+Vérifiez la valeur du SMC applicable dans le dernier avenant à la convention collective puis renseignez le champ.`,
       },
+    },
+    {
+      type: "regulatory",
+      content: `Lorsque l'apprenti a 21 ans ou plus, si le SMC existe et que son montant est plus avantageux pour l'apprenti, son application est obligatoire.`,
     },
     {
       type: "bonus",
       content: `Le salaire minimum conventionnel (SMC) est le salaire minimum prévu par une convention collective de branche, issu d’une « grille de classification », applicable à un salarié.
-      
-Dans certains secteurs, il existe aussi des majorations prévues par la convention collective.`,
-      collapse: {
-        label: "En savoir plus",
-        content: `Si vous ne connaissez pas le SMC applicable, consultez votre convention collective.
 
-L'information figure en général dans la partie "Salaires". Vous devez vous référer à la position du poste occupé par l'apprenti dans la classification hiérarchique pour  déterminer la rémunération minimum applicable à l’apprenti à partir du salaire de base et du pourcentage appliqué.`,
-      },
+Dans certains secteurs, il existe aussi des majorations prévues par la convention collective.`,
     },
   ];
 };
