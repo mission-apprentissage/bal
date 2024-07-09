388585fb41226f7dbf7d5254c233558f45cbfcac
Alan Le Ruyet <alan.leruyet@free.fr>
2024-07-09 14:52:32 +0200

    Update server/src/modules/jobs/deca/hydrate-deca.ts
    Co-authored-by: Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>

--- server/src/modules/jobs/deca/hydrate-deca.ts
+++ server/src/modules/jobs/deca/hydrate-deca.ts
@@ -25,7 +25,9 @@ function getMaxOldestDateForFetching() {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-const ifDefined = (key: string, value: any, transform = (v: any) => v) => {
+type ITransform<T, V> = (v: NonNullable<T>) => V
+const identity:  ITransform<T, T> = v => v
+const ifDefined = <T, V>(key: string, value: T, transform: ITransform<T, V> = identity): NonNullable<T> => v) => {
   return value ? { [key]: transform(value) } : {};
 };
 
