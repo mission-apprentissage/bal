1cc6209a213d9b9dd187daa7eec5dc476e176417
Kevin Barnoin <k.barnoin@gmail.com>
2024-07-24 14:50:16 +0200

    fix: copy/paste 3

--- .bin/commands.sh
+++ .bin/commands.sh
@@ -23,10 +23,10 @@ function Help() {
 }
 
 function bin:setup() {
-  sudo ln -fs "${ROOT_DIR}/.bin/mna" "/usr/local/bin/mna-bal"
+  sudo ln -fs "${ROOT_DIR}/.bin/mna-bal" /usr/local/bin/mna-bal
 
   sudo mkdir -p /usr/local/share/zsh/site-functions
-  sudo ln -fs "${ROOT_DIR}/.bin/zsh-completion" "/usr/local/share/zsh/site-functions/_mna-bal"
+  sudo ln -fs "${ROOT_DIR}/.bin/zsh-completion" /usr/local/share/zsh/site-functions/_mna-bal
   sudo rm -f ~/.zcompdump*
 }
 
