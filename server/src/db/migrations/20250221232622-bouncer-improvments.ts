f0db154f4b97548a0e5538cee9e6133d7ed425a0
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-02-22 23:35:15 +0100

    fix: bouncer edge cases

---
+++ server/src/db/migrations/20250221232622-bouncer-improvments.ts
@@ -0,0 +1,9 @@
+import { Db, MongoClient } from "mongodb";
+
+import { getDbCollection } from "../../common/utils/mongodbUtils";
+
+export const up = async (_db: Db, _client: MongoClient) => {
+  await getDbCollection("bouncer.domain").deleteMany({
+    "ping.status": { $in: ["error", "not_supported"] },
+  });
+};
