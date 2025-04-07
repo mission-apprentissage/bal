9126c3d0bfb4fd5f065011fbae7aa4205b8c4d4e
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-04-07 10:07:12 +0200

    fix: lba training links API

--- server/src/common/apis/lba.ts
+++ server/src/common/apis/lba.ts
@@ -1,37 +1,34 @@
-import { AxiosInstance } from "axios";
+import { isAxiosError } from "axios";
 
 import config from "@/config";
 
-import { ApiError, apiRateLimiter } from "../utils/apiUtils";
+import { ApiError } from "../utils/apiUtils";
 import getApiClient from "./client";
 
 export const LIMIT_TRAINING_LINKS_PER_REQUEST = 1_000;
 
-const executeWithRateLimiting = apiRateLimiter("mailingList", {
-  //3 requests per second
-  nbRequests: 3,
-  durationInSeconds: 1,
-  maxQueueSize: LIMIT_TRAINING_LINKS_PER_REQUEST,
-  client: getApiClient(
-    {
-      baseURL: config.lba.baseURL,
-      timeout: 0, // no timeout
-    },
-    { cache: false }
-  ),
-});
+const client = getApiClient(
+  {
+    baseURL: config.lba.baseURL,
+    timeout: 0, // no timeout
+  },
+  { cache: false }
+);
 
 export interface TrainingLinkData {
   id: string;
-  cle_ministere_educatif?: string;
-  mef?: string;
-  cfd?: string;
-  rncp?: string;
-  code_postal?: string;
-  uai_lieu_formation?: string;
-  uai_formateur?: string;
-  uai_formateur_responsable?: string;
-  code_insee?: string;
+  cle_ministere_educatif?: string | null;
+  mef?: string | null;
+  cfd?: string | null;
+  rncp?: string | null;
+  code_postal?: string | null;
+  uai_lieu_formation?: string | null;
+  uai_formateur?: string | null;
+  uai_formateur_responsable?: string | null;
+  code_insee?: string | null;
+  siret_lieu_formation?: string | null;
+  siret_formateur?: string | null;
+  siret_formateur_responsable?: string | null;
 }
 
 export interface TrainingLink {
@@ -41,16 +38,18 @@ export interface TrainingLink {
 }
 
 export const getTrainingLinks = async (data: TrainingLinkData[]): Promise<TrainingLink[]> => {
-  return executeWithRateLimiting(async (client: AxiosInstance) => {
-    console.log(`Request fired with ${data.length} items`);
-    try {
-      const { data: links } = await client.post<TrainingLink[]>(`/api/traininglinks`, data);
-      console.log(`Request success with ${links.length} items`);
+  console.log(`Request fired with ${data.length} items`);
+  try {
+    const { data: links } = await client.post<TrainingLink[]>(`/api/traininglinks`, data);
+    console.log(`Request success with ${links.length} items`);
 
-      return links;
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    } catch (error: any) {
-      throw new ApiError("Api LBA", `${error.message}`, error.code || error.response?.status);
+    return links;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    if (isAxiosError(error)) {
+      throw new ApiError("Api LBA", `${error.code}: ${error.message}`, JSON.stringify(error.response?.data ?? null));
     }
-  });
+
+    throw new ApiError("Api LBA", `${error.message}`, error.code || error.response?.status);
+  }
 };
