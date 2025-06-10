d9a78cf002fb9b3011bdb4700f6c1533af24ad47
Moroine Bentefrit <moroine.bentefrit@beta.gouv.fr>
2025-06-10 16:29:29 +0200

    feat: add bouncer ttl

---
+++ server/src/db/migrations/20250610142544-bouncer-ttl.ts
@@ -0,0 +1,30 @@
+import { Db, MongoClient } from "mongodb";
+
+import { getDbCollection } from "../../common/utils/mongodbUtils";
+
+export const up = async (_db: Db, _client: MongoClient) => {
+  await getDbCollection("bouncer.email").updateMany(
+    {
+      "ping.status": { $ne: "invalid" },
+      ttl: { $exists: false },
+    },
+    {
+      $set: {
+        ttl: null,
+      },
+    },
+    { bypassDocumentValidation: true }
+  );
+
+  await getDbCollection("bouncer.email").updateMany(
+    {
+      "ping.status": "invalid",
+    },
+    {
+      $set: {
+        ttl: new Date(Date.now() + 30 * 24 * 60 * 60 * 1000), // Set TTL to 30 days from now
+      },
+    },
+    { bypassDocumentValidation: true }
+  );
+};
