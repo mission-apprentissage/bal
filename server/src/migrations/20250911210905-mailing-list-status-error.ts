766d8aa3acc1ae8beb60b2c3b05c695883ccbf80
Alan Le Ruyet <alan.leruyet@free.fr>
2025-09-25 17:41:38 +0200

    feat: adding message
    Added logging to track the start and end of the migration process.

--- server/src/migrations/20250911210905-mailing-list-status-error.ts
+++ server/src/migrations/20250911210905-mailing-list-status-error.ts
@@ -1,6 +1,8 @@
 import { getDbCollection } from "../common/utils/mongodbUtils";
 
 export const up = async () => {
+  console.log("starting 20250911210905-mailing-list-status-error")
+  
   await getDbCollection("mailingListsV2").updateMany(
     { error: { $exists: false } },
     { $set: { error: null } },
@@ -11,6 +13,8 @@ export const up = async () => {
     { $set: { eta: null } },
     { bypassDocumentValidation: true }
   );
+  
+  console.log("ended 20250911210905-mailing-list-status-error")
 };
 
 export const requireShutdown: boolean = true;
