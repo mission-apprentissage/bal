96a5d1445b0964e60613eb99a3e346cf085bf6b5
Alan Le Ruyet <alan.leruyet@beta.gouv.fr>
2025-10-02 12:17:02 +0200

    fix: prettier and comment

--- server/src/migrations/20251002113947-date-signature-contrat-null.ts
+++ server/src/migrations/20251002113947-date-signature-contrat-null.ts
@@ -1,9 +1,13 @@
 import type { Db } from "mongodb";
+import logger from "../common/logger";
 
 export const up = async (db: Db) => {
+  logger.info("Migration 20251002113947-date-signature-contrat-null started");
   await db
     .collection("deca")
     .updateMany({}, { $set: { date_signature_contrat: null } }, { bypassDocumentValidation: true });
+
+  logger.info("Migration 20251002113947-date-signature-contrat-null completed");
 };
 
 export const requireShutdown: boolean = false;
