77cddaead8b568acc49c3eeecceab3c229393c28
mathieu <mathieu.borrell@beta.gouv.fr>
2026-01-20 21:28:10 +0100

    fix: homogénéisation des scripts mutualisés (#499)

--- .bin/scripts/bin-setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-sudo ln -fs "${ROOT_DIR}/.bin/mna-bal" /usr/local/bin/mna-${PRODUCT_NAME}
-
-sudo mkdir -p /usr/local/share/zsh/site-functions
-sudo ln -fs "${ROOT_DIR}/.bin/zsh-completion" /usr/local/share/zsh/site-functions/_mna-${PRODUCT_NAME}
-sudo rm -f ~/.zcompdump*
