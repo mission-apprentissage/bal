4f5dee119a12104e0bbf88ccc2881b1aa2a7e8d3
mathieu <mathieu.borrell@beta.gouv.fr>
2026-01-20 22:03:44 +0100

    fix: correction sur la chaîne de déploiement (#500)

--- release.config.js
+++ release.config.js
@@ -7,7 +7,7 @@ module.exports = {
     [
       "@semantic-release/exec",
       {
-        prepareCmd: `.bin/mna-bal release:app \${nextRelease.version} push`,
+        prepareCmd: `.bin/mna-bal app:release \${nextRelease.version} push`,
       },
     ],
     "@semantic-release/github",
